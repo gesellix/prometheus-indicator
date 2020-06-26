fcbd9772d596105ccc1e7a1702158f8f59747610
Tobias Gesellchen <tobias@gesellix.de>
2020-06-26 18:57:55 +0200

    adopt alertmanager v2 api

--- src/prometheus-alerts.js
+++ src/prometheus-alerts.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
   "use strict";
   const _ = require('lodash');
   const Q = require('q');
@@ -11,12 +11,13 @@
         return cb(null, "Please specify Prometheus Alertmanager base URI (alertmanager.baseUri).")
       }
 
-      const requestPrometheusAlertmanager = function() {
+      const requestPrometheusAlertmanager = function () {
         const deferred = Q.defer();
-        jsonRequest(alertmanager.baseUri + "/api/v1/alerts/groups", function(error, json) {
+        jsonRequest(alertmanager.baseUri + "/api/v2/alerts?silenced=false&inhibited=false", function (error, json) {
           if (error) {
             deferred.reject(error);
-          } else {
+          }
+          else {
             deferred.resolve(json);
           }
         });
@@ -24,48 +25,55 @@
       };
 
       requestPrometheusAlertmanager().then(
-        function(jsonResponse) {
-          const alerts = collectAlerts(jsonResponse.data);
-          console.log('current alerts', alerts);
-
-          const status = {
-            title: (alertmanager.title || ""),
-            status: 'ok',
-            alerts: []
-          };
-          const criticalAlerts = _.filter(alerts, {
-            severity: 'critical',
-            silenced: false
-          });
-          const warningAlerts = _.filter(alerts, {
-            severity: 'warning',
-            silenced: false
-          });
-
-          if (!_.isEmpty(criticalAlerts)) {
-            status.status = 'critical';
-            status.alerts = convertToMessages(criticalAlerts)
-          } else if (!_.isEmpty(warningAlerts)) {
-            status.status = 'warning';
-            status.alerts = convertToMessages(warningAlerts)
-          }
-          cb(status, null);
+        function (jsonResponse) {
+          var summary = _
+            .chain(jsonResponse)
+            .groupBy(function (alert) {
+              if (alert.labels.severity === 'critical' || _.includes(['P1', 'P2'], alert.labels.priority)) {
+                return 'critical';
+              }
+              else if (alert.labels.severity === 'warning' || _.includes(['P3', 'P4', 'P5'], alert.labels.priority)) {
+                return 'warning';
+              }
+            })
+            .thru(function (alertsBySeverity) {
+              var criticals = alertsBySeverity['critical'];
+              if (!_.isEmpty(criticals)) {
+                return criticals;
+              }
+              var warnings = alertsBySeverity['warning'];
+              if (!_.isEmpty(warnings)) {
+                return warnings;
+              }
+              return [];
+            })
+            .reduce(function (status, alert) {
+              status.status = alert.labels.severity || 'warning';
+              status.alerts.push(convertToMessage(alert));
+              return status;
+            }, {
+              status: 'ok',
+              alerts: [],
+            })
+            .value();
+          cb(summary, null);
         },
-        function(error) {
+        function (error) {
           console.log('error:', error);
           cb(null, "can't get prometheus alertmanager alerts");
         }
-      );
+      )
 
       function jsonRequest(options, callback) {
-        request(options, function(err, response, body) {
-          if (err || !response || response.statusCode != 200) {
+        request(options, function (err, response, body) {
+          if (err || !response || response.statusCode !== 200) {
             err = (err || (response ? ("bad statusCode: " + response.statusCode) : "bad response")) + " from " + options.url;
           }
           let jsonBody;
           try {
             jsonBody = JSON.parse(body);
-          } catch (e) {
+          }
+          catch (e) {
             if (!err) {
               err = 'invalid json response';
             }
@@ -74,31 +82,17 @@
         });
       }
 
-      function collectAlerts(json) {
-        return _.flattenDeep(_.map(json, function(d) {
-          return _.map(d.blocks, function(block) {
-            return _.map(block.alerts, function(alert) {
-              return {
-                "severity": alert.labels.severity,
-                "env": alert.labels.env,
-                "alertname": alert.labels.alertname,
-                "human_readable_context_name": alert.labels.human_readable_context_name,
-                "silenced": alert.hasOwnProperty('silenced')
-              };
-            });
-          });
-        }));
-      }
-
-      function convertToMessages(alerts) {
-        return _(alerts).map(convertToMessage).value();
-      }
-
       function convertToMessage(alert) {
-        return [
-          (alert.env || '').toUpperCase(),
-          alert.alertname,
-          alert.human_readable_context_name].join(" ").trim();
+        if (alert.annotations && alert.annotations['teamwall_summary']) {
+          return alert.annotations['teamwall_summary'];
+        }
+        else {
+          return [
+            alert.labels.env ? alert.labels.env.toUpperCase() : '',
+            alert.labels.alertname,
+            alert.labels.human_readable_context_name
+          ].join(' ').trim();
+        }
       }
     }
   };
